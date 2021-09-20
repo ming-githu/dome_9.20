dd165a23fe65b2e713432f905a4ed3b98ccc8d65
ming-githu <1044364828@qq.com>
2021-09-20 13:07:56 +0800

    类型转换

--- src/demo9_20/lin9_20.java
+++ src/demo9_20/lin9_20.java
@@ -1,7 +1,11 @@
 package demo9_20;
 
+//类型转换
 public class lin9_20 {
     public static void main(String[] args) {
-        System.out.println("222");
+//        System.out.println("222");
+//        自动转换
+        double d = 10;
+        System.out.println(d);
     }
 }
