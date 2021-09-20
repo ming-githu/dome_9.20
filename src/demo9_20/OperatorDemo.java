fe657eeb177fa0f92af809dbaeee0d2ff20ad10b
ming-githu <1044364828@qq.com>
2021-09-20 16:25:51 +0800

    lin

--- src/demo9_20/OperatorDemo.java
+++ src/demo9_20/OperatorDemo.java
@@ -2,9 +2,10 @@ package demo9_20;
 
 public class OperatorDemo {
     public static void main(String[] args) {
-//        逻辑运算符与：&,或：|
+//        逻辑运算符与：&,或：| , 非：！
         int a = 1, b = 2, c = 3;
         System.out.println(a == 1 & b == 1);//false
         System.out.println(b == 2 | c == 2);//ture
+        System.out.println(!(a == 2));//ture
     }
 }
