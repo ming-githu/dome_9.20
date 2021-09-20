f72d8d8c84115cee6bf6300045cbdb0f91819141
ming-githu <1044364828@qq.com>
2021-09-20 16:12:20 +0800

    lin

--- src/demo9_20/lin9_20.java
+++ src/demo9_20/lin9_20.java
@@ -9,7 +9,7 @@ public class lin9_20 {
         System.out.println(d);//10.0
 //        强制转换
 //        把一个表示数据范围大的赋值给表示数据范围小的变量
-        int i = (int) d;
-        System.out.println(i);//10
+        int i = (int) 8.88;
+        System.out.println(i);//8会发生数据丢失，不建议使用
     }
 }
