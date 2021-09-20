1e6fce2123c67856d30d6acf912fa4946c8f3958
ming-githu <1044364828@qq.com>
2021-09-20 16:24:01 +0800

    lin

---
+++ src/demo9_20/OperatorDemo.java
@@ -0,0 +1,10 @@
+package demo9_20;
+
+public class OperatorDemo {
+    public static void main(String[] args) {
+//        逻辑运算符与：&,或：|
+        int a = 1, b = 2, c = 3;
+        System.out.println(a == 1 & b == 1);//false
+        System.out.println(b == 2 | c == 2);//ture
+    }
+}
