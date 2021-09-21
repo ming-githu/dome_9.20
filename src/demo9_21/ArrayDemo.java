0dede9522484c398d537b0f93fc4b30d715c57f3
ming-githu <1044364828@qq.com>
2021-09-21 12:28:23 +0800

    数组使用

---
+++ src/demo9_21/ArrayDemo.java
@@ -0,0 +1,12 @@
+package demo9_21;
+
+public class ArrayDemo {
+    public static void main(String[] args) {
+        int arr[] = new int[5];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i;
+            System.out.print(arr[i] + ",");
+        }
+
+    }
+}
