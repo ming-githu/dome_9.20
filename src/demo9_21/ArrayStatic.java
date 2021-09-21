a02bba16702a953c2589efc7afb7492c2d6038e4
ming-githu <1044364828@qq.com>
2021-09-21 13:44:43 +0800

    冒泡排序法

--- src/demo9_21/ArrayStatic.java
+++ src/demo9_21/ArrayStatic.java
@@ -2,9 +2,17 @@ package demo9_21;
 
 public class ArrayStatic {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5, 6};
+        int[] arr = {1, 2, 30, 4, 5, 6};
         System.out.println(arr);
         for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                int num;
+                if (arr[j] > arr[i]) {
+                    num = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = num;
+                }
+            }
             System.out.print(arr[i] + ",");
         }
     }
