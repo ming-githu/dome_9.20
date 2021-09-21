95b6587998566cc0edb89dee6108c3e426387fd6
ming-githu <1044364828@qq.com>
2021-09-21 13:52:34 +0800

    冒泡排序优化

--- src/demo9_21/ArrayStatic.java
+++ src/demo9_21/ArrayStatic.java
@@ -3,7 +3,14 @@ package demo9_21;
 public class ArrayStatic {
     public static void main(String[] args) {
         int[] arr = {1, 2, 30, 4, 5, 6};
+        ArrayStatic a = new ArrayStatic();
+        arr = a.arrs(arr);//调用数组排序的方法
         System.out.println(arr);
+
+    }
+
+    //    定义数组排序的方法
+    public int[] arrs(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             for (int j = i + 1; j < arr.length; j++) {
                 int num;
@@ -15,5 +22,6 @@ public class ArrayStatic {
             }
             System.out.print(arr[i] + ",");
         }
+        return arr;
     }
 }
