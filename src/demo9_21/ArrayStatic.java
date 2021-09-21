ff909fee18f6cf151a42bd6cbfa26377e417eab0
ming-githu <1044364828@qq.com>
2021-09-21 14:07:34 +0800

    l

--- src/demo9_21/ArrayStatic.java
+++ src/demo9_21/ArrayStatic.java
@@ -6,11 +6,28 @@ public class ArrayStatic {
         ArrayStatic a = new ArrayStatic();
         arr = a.arrs(arr);//调用数组排序的方法
         System.out.println(arr);
-
+//        使用static定义的方法不需要new出来一个对象就可以直接使用
+        arrs2(arr);
     }
 
     //    定义数组排序的方法
     public int[] arrs(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                int num;
+                if (arr[j] > arr[i]) {
+                    num = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = num;
+                }
+            }
+            System.out.print(arr[i] + ",");
+        }
+        return arr;
+    }
+
+    //    定义数组排序的方法 使用static定义的
+    public static int[] arrs2(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             for (int j = i + 1; j < arr.length; j++) {
                 int num;
